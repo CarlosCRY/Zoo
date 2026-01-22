77e1f9ee8b989ea36fd9c511308da26eca8852b1
CarlosCRY <trueccr@gmail.com>
2026-01-22 11:03:51 +0100

    Gaooo

--- src/main/java/org/ies/tierno/readers/random/AreaRanDer.java
+++ src/main/java/org/ies/tierno/readers/random/AreaRanDer.java
@@ -14,13 +14,13 @@ public class AreaRanDer <T extends Animal> implements Reader<Area<T>> {
 
     @Override
     public Area<T> read() {
-        T[] animals = (T[]) new Animal[random.nextInt(2-10)];
+        T[] animals = (T[]) new Animal[random.nextInt(2, 10)];
         for (int i = 0; i < animals.length; i++) {
             animals[i] = aniReader.read();
         }
 
         return new Area<> (
-                random.nextInt(10-100),
+                random.nextInt(10, 100),
                 animals
         );
     }
